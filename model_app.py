91a2407f592a9b01114ea5c9fb905fd8e515f943
kafiirgie <kafiirgie20@gmail.com>
2022-11-24 00:03:00 +0700

    update features

--- model_app.py
+++ model_app.py
@@ -29,10 +29,24 @@ class Model():
         self.conn.close()
 
     def insert_kegiatan(self, kegiatan):
-        pass  
+        self.conn = sqlite3.connect('sibukin.db')
+        self.c = self.conn.cursor()
+        with self.conn:
+            self.c.execute("""INSERT INTO kegiatan VALUES
+                        (:id_kegiatan, :nama_kegiatan, :batas_waktu, :status, :id_kategori)""",
+                        {'id_kegiatan': kegiatan.id, 'nama_kegiatan': kegiatan.nama, 'batas_waktu': kegiatan.waktu, 'status': kegiatan.status, 'id_kategori': kegiatan.kategori}
+                    )
+        self.conn.close()    
 
     def insert_kategori(self, kategori):
-        pass
+        self.conn = sqlite3.connect('sibukin.db')
+        self.c = self.conn.cursor()
+        with self.conn:
+            self.c.execute("""INSERT INTO kategori VALUES
+                        (:id_kategori, :nama_kategori)""",
+                        {'id_kategori': kategori.id, 'nama_kategori': kategori.nama}
+                    )
+        self.conn.close()
 
     def get_all_kegiatan(self):
         self.conn = sqlite3.connect('sibukin.db')
@@ -47,40 +61,89 @@ class Model():
         return self.c.fetchall()
     
     def get_kegiatan_filtered_today(self):
-        pass
+        self.conn = sqlite3.connect('sibukin.db')
+        self.c = self.conn.cursor()
+        self.c.execute("SELECT * FROM kegiatan INNER JOIN kategori USING(id_kategori) WHERE batas_waktu = Date('now')")
+        return self.c.fetchall()
     
     def get_kegiatan_filtered_status(self, status):
-        pass
+        self.conn = sqlite3.connect('sibukin.db')
+        self.c = self.conn.cursor()
+        # self.c.execute(f"SELECT * FROM kegiatan INNER JOIN kategori USING(id_kategori) WHERE status = '{status}'")
+        self.c.execute("SELECT * FROM kegiatan INNER JOIN kategori USING(id_kategori) WHERE status = :status", {'status': status})
+        return self.c.fetchall()
     
     def get_kegiatan_filtered_kategori(self, kategori):
-        pass
+        self.conn = sqlite3.connect('sibukin.db')
+        self.c = self.conn.cursor()
+        print(kategori)
+        self.c.execute(f"SELECT * FROM kegiatan INNER JOIN kategori USING(id_kategori) WHERE nama_kategori = '{kategori}'")
+        # self.c.execute("SELECT * FROM kegiatan INNER JOIN kategori USING(id_kategori) WHERE nama_kategori = :nama_kategori", {'nama_kategori':kategori})
+        return self.c.fetchall()
     
     def get_kegiatan_filtered_status_kategori(self, status, kategori):
-        pass
+        self.conn = sqlite3.connect('sibukin.db')
+        self.c = self.conn.cursor()
+        self.c.execute("SELECT * FROM kegiatan INNER JOIN kategori USING(id_kategori) WHERE status = :status AND nama_kategori = :nama_kategori", {'status':status,'nama_kategori':kategori})
+        return self.c.fetchall()
 
     def get_kegiatan_filtered_status_today(self, status):
-        pass
+        self.conn = sqlite3.connect('sibukin.db')
+        self.c = self.conn.cursor()
+        self.c.execute("SELECT * FROM kegiatan INNER JOIN kategori USING(id_kategori) WHERE batas_waktu = Date('now') AND status = :status", {'status':status})
+        return self.c.fetchall()
 
     def get_kegiatan_filtered_kategori_today(self, kategori):
-        pass
+        self.conn = sqlite3.connect('sibukin.db')
+        self.c = self.conn.cursor()
+        self.c.execute("SELECT * FROM kegiatan INNER JOIN kategori USING(id_kategori) WHERE batas_waktu = Date('now') nama_kategori = :nama_kategori", {'nama_kategori':kategori})
+        return self.c.fetchall()
     
     def get_kegiatan_filtered_status_kategori_today(self, status, kategori):
-        pass
+        self.conn = sqlite3.connect('sibukin.db')
+        self.c = self.conn.cursor()
+        self.c.execute("SELECT * FROM kegiatan INNER JOIN kategori USING(id_kategori) WHERE batas_waktu = Date('now') AND status = :status AND nama_kategori = :nama_kategori", {'status':status,'nama_kategori':kategori})
+        return self.c.fetchall()
 
     def get_all_kategori(self):
-        pass
+        self.conn = sqlite3.connect('sibukin.db')
+        self.c = self.conn.cursor()
+        self.c.execute("SELECT * FROM kategori")
+        return self.c.fetchall()
 
     def get_kegiatan_by_id(self, id):
-        pass
+        self.conn = sqlite3.connect('sibukin.db')
+        self.c = self.conn.cursor()
+        self.c.execute("SELECT * FROM kegiatan WHERE id_kegiatan=:id_kegiatan", {'id_kegiatan': id})
+        return self.c.fetchone()
 
     def get_kategori_by_id(self, id):
-        pass
+        self.conn = sqlite3.connect('sibukin.db')
+        self.c = self.conn.cursor()
+        self.c.execute("SELECT nama_kategori FROM kategori WHERE id_kategori=:id_kategori", {'id_kategori': id})
+        return self.c.fetchone()
 
     def update_status(self, id, new_status):
-        pass
+        self.conn = sqlite3.connect('sibukin.db')
+        self.c = self.conn.cursor()
+        with self.conn:
+            self.c.execute("""UPDATE kegiatan SET status = :status
+                        WHERE id_kegiatan = :id_kegiatan""",
+                    {'id_kegiatan': id, 'status': new_status})
+        self.conn.close()
 
     def remove_kegiatan(self, kegiatan):
-        pass
+        self.conn = sqlite3.connect('sibukin.db')
+        self.c = self.conn.cursor()
+        with self.conn:
+            self.c.execute("DELETE from kegiatan WHERE id_kegiatan = :id_kegiatan",
+                    {'id_kegiatan': kegiatan.id})
+        self.conn.close()
     
     def remove_kegiatan_by_id(self, id):
-        pass
+        self.conn = sqlite3.connect('sibukin.db')
+        self.c = self.conn.cursor()
+        with self.conn:
+            self.c.execute("DELETE from kegiatan WHERE id_kegiatan = :id_kegiatan",
+                    {'id_kegiatan': id})
+        self.conn.close()
